bbcf84d676cc235d15cfdf6221f5d8ae904188b2
mr.p.kharitonov <mr.p.kharitonov@gmail.com>
2021-12-22 22:45:41 +0600

    add main.go

---
+++ main.go
@@ -0,0 +1,5 @@
+package main // говорим, что файл принадлежит пакету main
+// func -- объявление функции
+func main() { // func main() -- точка входа
+	println("Hello, world !") // println -- функция, выводящая в консоль строку
+}
