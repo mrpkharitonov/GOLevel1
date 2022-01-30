9921b3df724dd706aff2de37c0ab3886e68a069f
mr.p.kharitonov <mr.p.kharitonov@gmail.com>
2022-01-30 16:36:11 +0600

    modify main.go

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-// заплатка в виду занятости на НГ праздники
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var fibonacciCache = make(map[uint]uint)
+
+func fibbonachi(n uint) uint {
+	var value uint
+
+	if fibonacciCache[n] != 0 {
+		value = fibonacciCache[n]
+	} else if n < 2 {
+		value = n
+	} else {
+		value = fibbonachi(n-1) + fibbonachi(n-2)
+	}
+	fibonacciCache[n] = value
+	return value
+}
+
+func main() {
+	val := 56
+	fmt.Println(time.Now())
+	fmt.Println(fibbonachi(uint(val)))
+	fmt.Println(time.Now())
+	fmt.Println(fibbonachi(uint(val)))
+	fmt.Println(time.Now())
+}
